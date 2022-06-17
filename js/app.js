66fbb10a7133193eccc091f1fdc58a9dabee6118
Alejandro Rivera <alexrivera78@gmail.com>
2022-06-17 17:44:23 -0500

    created practice page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// function Dog (name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// let ageDiff = function(dog1, dog2) {
+//   return Math.abs(dog1.age - dog2.age);
+// };
+
+// ageDiff(new Dog('Sparky', 12), new Dog('Molly', 8));
+
+// console.log();
+
+
+
+
+
+// let movie = { title: 'The Matrix' };
+// typeof(typeof(movie));
+
+// console.log(typeof(typeof(movie)));
+
+
+
